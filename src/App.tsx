3d8e4e0fa0a220fdf3affa93540d8ded5a0d3d0f
Albert Celestino <162110077+1lbert@users.noreply.github.com>
2024-05-18 15:46:07 +0800

    initialize commit

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import quotes from "./assets/quotes.json";
+import {FaTwitter, FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
+import './App.css';
+
+interface Quote { 
+  quote: string;  
+  author: string;
+}
+
+const getRandomQuote = (): Quote => { 
+  return quotes[Math.floor(Math.random() * quotes.length)];
+};
+
+const getRandomColor = (): string => { 
+  const red = Math.floor(Math.random() * 128);
+  const green = Math.floor(Math.random() * 128);
+  const blue = Math.floor(Math.random() * 128);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+
+const transition = "all 1s";
+
+function App() {
+  const [quote, setQuote] = useState<Quote>(getRandomQuote());
+  const [randomColor, setRandomColor] = useState<string>((getRandomColor));
+
+  const changeQuote = () => { 
+    setQuote(getRandomQuote());
+    setRandomColor(getRandomColor());
+  }
+
+  return (
+    <div className="background" style={{backgroundColor: randomColor, transition}}>
+      <div id="quote-box">
+        <div className="quote-content" style={{color: randomColor, transition}}>
+          <h2 id="text">
+            <FaQuoteLeft size="30" style={{marginRight: "10px"}}/>
+            {quote.quote}
+            <FaQuoteRight size="30" style={{marginLeft: "10px"}}/>
+          </h2>
+          <h4 id="author"> - {quote.author}</h4>
+        </div>
+        <div className="buttons">
+          <a 
+            href={""} 
+            id="tweet-quote" 
+            style={{
+              backgroundColor: randomColor, 
+              transition,
+              marginRight: "10px"
+              }}
+          >
+            <FaTwitter color="white" />
+          </a>
+          <button 
+            id="new-quote" 
+            onClick={changeQuote} 
+            style={{backgroundColor: randomColor, transition}}> 
+            Change Quote 
+          </button>
+        </div>
+      </div>
+      <footer className="footer">
+        <h5>Created by: Albert Celestino | May 18, 2024</h5>
+        <h5 style={{textAlign: "center"}}>
+          Project on 
+          <span>
+            <a href="https://www.freecodecamp.org/" className='project-link' target='_blank'>
+              FreeCodeCamp
+            </a>
+          </span>
+        </h5>
+      </footer>
+    </div>
+    
+  );
+};
+
+export default App;
